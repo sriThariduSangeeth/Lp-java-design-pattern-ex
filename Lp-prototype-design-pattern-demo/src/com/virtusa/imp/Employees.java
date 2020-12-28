45d4131cc67e583a9532cf516a314f6516665e51
sriThariduSangeeth <dtsangeeth@gmail.com>
2020-12-28 13:59:03 +0530

    create Prototype classes

---
+++ Lp-prototype-design-pattern-demo/src/com/virtusa/imp/Employees.java
@@ -0,0 +1,42 @@
+package com.virtusa.imp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author dtsangeeth
+ * @created 28 / 12 / 2020
+ * @project Lp-prototype-design-pattern-demo
+ */
+public class Employees implements Cloneable{
+
+    private List<String> empList;
+
+    public Employees(){
+        empList = new ArrayList<String>();
+    }
+
+    public Employees(List<String> list){
+        this.empList=list;
+    }
+    public void loadData(){
+        //read all employees from database and put into the list
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("David");
+        empList.add("Lisa");
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+        List<String> temp = new ArrayList<String>();
+        for(String s : this.getEmpList()){
+            temp.add(s);
+        }
+        return new Employees(temp);
+    }
+}
--- Lp-prototype-design-pattern-demo/src/com/virtusa/main/Application.java
+++ Lp-prototype-design-pattern-demo/src/com/virtusa/main/Application.java
@@ -1,9 +1,41 @@
 package com.virtusa.main;
 
+import com.virtusa.imp.Employees;
+
+import java.util.List;
+
 /**
  * @author dtsangeeth
  * @created 28 / 12 / 2020
  * @project Lp-prototype-design-pattern-demo
  */
 public class Application {
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.display();
+    }
+
+    public void display(){
+
+        try{
+            Employees emps = new Employees();
+            emps.loadData();
+
+            //Use the clone method to get the Employee object
+            Employees empsNew = (Employees) emps.clone();
+            Employees empsNew1 = (Employees) emps.clone();
+            List<String> list = empsNew.getEmpList();
+            list.add("John");
+            List<String> list1 = empsNew1.getEmpList();
+            list1.remove("Pankaj");
+
+            System.out.println("emps List: "+emps.getEmpList());
+            System.out.println("empsNew List: "+list);
+            System.out.println("empsNew1 List: "+list1);
+
+        }catch (CloneNotSupportedException ex){
+            System.out.println(""+ex);
+        }
+    }
 }
